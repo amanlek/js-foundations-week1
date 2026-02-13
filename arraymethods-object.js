c346401a0fc3a0245b9fc68e46a5d9676e0fbd14
amanlek <amanlekharajani20@gmail.com>
2026-02-13 18:14:25 +0530

    Day-2 Array Methods and Object operations

--- arraymethods-object.js
+++ arraymethods-object.js
@@ -1,82 +1,259 @@
 const users = [
- {id:1,name:"Aman",role:"Developer",active:true,salary:60000,address:{city:"Pune",state:"MH"}},
- {id:2,name:"Rohit",role:"Tester",active:false,salary:40000,address:{city:"Mumbai",state:"MH"}},
- {id:3,name:"Sneha",role:"Developer",active:true,salary:65000,address:{city:"Bengaluru",state:"KA"}},
- {id:4,name:"Priya",role:"Manager",active:false,salary:90000,address:{city:"Delhi",state:"DL"}},
- {id:5,name:"Karan",role:"Tester",active:true,salary:42000,address:{city:"Ahmedabad",state:"GJ"}},
- {id:6,name:"Neha",role:"HR",active:false,salary:50000,address:{city:"Jaipur",state:"RJ"}},
- {id:7,name:"Arjun",role:"Developer",active:false,salary:70000,address:{city:"Hyderabad",state:"TS"}},
- {id:8,name:"Pooja",role:"HR",active:true,salary:52000,address:{city:"Indore",state:"MP"}},
- {id:9,name:"Vikas",role:"Manager",active:false,salary:88000,address:{city:"Chandigarh",state:"PB"}},
- {id:10,name:"Anjali",role:"Tester",active:false,salary:45000,address:{city:"Nagpur",state:"MH"}},
- {id:11,name:"Rahul",role:"Developer",active:true,salary:68000,address:{city:"Noida",state:"UP"}},
- {id:12,name:"Meera",role:"HR",active:true,salary:null,address:{city:"Bhopal",state:"MP"}},
- {id:13,name:"Suresh",role:"Developer",active:false,salary:62000,address:{city:"Chennai",state:"TN"}},
- {id:14,name:"Isha",role:"Tester",active:false,salary:43000,address:{city:"Surat",state:"GJ"}},
- {id:15,name:"Nikhil",role:"Manager",active:true,salary:95000,address:{city:"Kolkata",state:"WB"}},
- {id:16,name:"Kavya",role:"Developer",active:false,salary:66000,address:{city:"Udaipur",state:"RJ"}},
- {id:17,name:"Amit",role:"Tester",active:false,salary:41000,address:{city:"Patna",state:"BR"}},
- {id:18,name:"Riya",role:"HR",active:true,salary:53000,address:{city:"Gwalior",state:"MP"}},
- {id:19,name:"Deepak",role:"Developer",active:true,salary:72000,address:{city:"Faridabad",state:"HR"}},
- {id:20,name:"Simran",role:"Manager",active:false,salary:98000,address:{city:"Gurgaon",state:"HR"}}
+  {
+    id: 1,
+    name: "Aman",
+    role: "Developer",
+    active: true,
+    salary: 60000,
+    address: { city: "Pune", state: "MH" },
+  },
+  {
+    id: 2,
+    name: "Rohit",
+    role: "Tester",
+    active: false,
+    salary: 40000,
+    address: { city: "Mumbai", state: "MH" },
+  },
+  {
+    id: 3,
+    name: "Sneha",
+    role: "Developer",
+    active: true,
+    salary: 65000,
+    address: { city: "Bengaluru", state: "KA" },
+  },
+  {
+    id: 4,
+    name: "Priya",
+    role: "Manager",
+    active: false,
+    salary: 90000,
+    address: { city: "Delhi", state: "DL" },
+  },
+  {
+    id: 5,
+    name: "Karan",
+    role: "Tester",
+    active: true,
+    salary: 42000,
+    address: { city: "Ahmedabad", state: "GJ" },
+  },
+  {
+    id: 6,
+    name: "Neha",
+    role: "HR",
+    active: false,
+    salary: 50000,
+    address: { city: "Jaipur", state: "RJ" },
+  },
+  {
+    id: 7,
+    name: "Arjun",
+    role: "Developer",
+    active: false,
+    salary: 70000,
+    address: { city: "Hyderabad", state: "TS" },
+  },
+  {
+    id: 8,
+    name: "Pooja",
+    role: "HR",
+    active: true,
+    salary: 52000,
+    address: { city: "Indore", state: "MP" },
+  },
+  {
+    id: 9,
+    name: "Vikas",
+    role: "Manager",
+    active: false,
+    salary: 88000,
+    address: { city: "Chandigarh", state: "PB" },
+  },
+  {
+    id: 10,
+    name: "Anjali",
+    role: "Tester",
+    active: false,
+    salary: 45000,
+    address: { city: "Nagpur", state: "MH" },
+  },
+  {
+    id: 11,
+    name: "Rahul",
+    role: "Developer",
+    active: true,
+    salary: 68000,
+    address: { city: "Noida", state: "UP" },
+  },
+  {
+    id: 12,
+    name: "Meera",
+    role: "HR",
+    active: true,
+    salary: null,
+    address: { city: "Bhopal", state: "MP" },
+  },
+  {
+    id: 13,
+    name: "Suresh",
+    role: "Developer",
+    active: false,
+    salary: 62000,
+    address: { city: "Chennai", state: "TN" },
+  },
+  {
+    id: 14,
+    name: "Isha",
+    role: "Tester",
+    active: false,
+    salary: 43000,
+    address: { city: "Surat", state: "GJ" },
+  },
+  {
+    id: 15,
+    name: "Nikhil",
+    role: "Manager",
+    active: true,
+    salary: 95000,
+    address: { city: "Kolkata", state: "WB" },
+  },
+  {
+    id: 16,
+    name: "Kavya",
+    role: "Developer",
+    active: false,
+    salary: 66000,
+    address: { city: "Udaipur", state: "RJ" },
+  },
+  {
+    id: 17,
+    name: "Amit",
+    role: "Tester",
+    active: false,
+    salary: 41000,
+    address: { city: "Patna", state: "BR" },
+  },
+  {
+    id: 18,
+    name: "Riya",
+    role: "HR",
+    active: true,
+    salary: 53000,
+    address: { city: "Gwalior", state: "MP" },
+  },
+  {
+    id: 19,
+    name: "Deepak",
+    role: "Developer",
+    active: true,
+    salary: 72000,
+    address: { city: "Faridabad", state: "HR" },
+  },
+  {
+    id: 20,
+    name: "Simran",
+    role: "Manager",
+    active: false,
+    salary: 98000,
+    address: { city: "Gurgaon", state: "HR" },
+  },
 ];
 
 
 
-
 //----------filter method
-const activeUsers = users.filter((user)=>user.active);   //this filters out the array to new array where users status is active, active = true.
+const activeUsers = users.filter((user) => user.active); //this filters out the array to new array where users status is active, active = true.
 console.log(activeUsers);
 /*we use filter method to filter out some elements from array based on some conditions*/
 
 
 
+/*
+*Note: we use map method when we have to perform any operation on each element of array and create a new array out of that, this does not update the old array
+*/
 //----------map method
-const userNames = activeUsers.map(user => user.name);         //this creates new array of extracted name property from active users
+const userNames = activeUsers.map((user) => user.name); //this creates new array of extracted name property from active users
 console.log(userNames);
-/*we use map method when we have to perform any operation on each element of array
-and create a new array out of that, this does not update the old array*/
 
 
 
-//-----------reduce method 
+//-----------reduce method
 //for sum, avg and group
 
 //SUM
-const totalSalary = users.reduce((res, curr) =>{
-   return res + (curr.salary||0); // This handles the edge case, if the salary is falsy(null, undefined) then the value would be taken as 0
-  }, 0); // 0 is the initial value and each current element is stored in res
-console.log(totalSalary);
+/**
+ * @param users - array of users
+ * @returns total salary of all users
+ */
+const getTotalSalary = (users) => {
+  return users.reduce((total, user) => total + (user.salary || 0), 0);
+};
+console.log(getTotalSalary(users));
+
+
 
 //Average
-const avgSalary = users.reduce((res, user, index, arr) => {  //it also demostrate use of value and index using reduce, here user->value
-  res += user.salary;             // keep adding salaries
-  if(index === arr.length -1){  
-    return res / arr.length;      // return average   
+/**
+ * @param users - array of user objects with a salary property
+ * @returns average salary of all users
+ * 
+ * Note: Demonstrates use of `reduce` with value, index, and array parameters.
+ */
+const getAvgSalary = (users) =>{
+  return users.reduce((res, user, index, arr) => {
+  
+  res += user.salary;     // Keep adding salaries
+  if (index === arr.length - 1) {     // If this is the last element, return the average
+    return res / arr.length;
   }
-  return res;                     // return running total
-}, 0); 
-console.log(avgSalary); 
+  // Otherwise, return running total
+  return res;
+}, 0);
+}
+console.log(getAvgSalary(users));
+
 
 //Grouping based on role
-const groupUser = users.reduce((group, user) => {
-  if(!group[user.role]){        // if role key(group) does not exist, create it
+/**
+ * @param users - array of user objects with `name` and `role` properties
+ * @returns an object grouping users by their role
+ * 
+ * Note: reduce method is used to get a single value from an array by computing some operations ont each element of array
+ */
+const groupUser = (users) => {
+  return users.reduce((group, user) => {
+  // If role key does not exist, create an empty array
+  if (!group[user.role]) {
     group[user.role] = [];
   }
-  group[user.role].push(user.name);  // push user name into role array
+  // Push user name into the array corresponding to their role
+  group[user.role].push(user.name);
+  // Return updated group object for next iteration
   return group;
-},{})
-console.log(groupUser);
-/*reduce method is used to get a single value from an
-array by computing some operations ont each element of array*/
+}, {});
+}
+console.log(groupUser(users));
 
 
 
-//--------chained operation 
-const totalSalaryActive = users.filter((user) => user.active).map((user) => user.salary).reduce((res, curr)=> res + curr);
-//here we calculated the total salary of active members only, first we filtered out active users and
-//then used map function to just include salary of active users and then used reduce method to count total salary
-console.log(totalSalaryActive);
+
+//--------chained operation
+/**
+ * @param users - array of user objects with `salary` and `active` properties
+ * @returns total salary of all active users
+ */
+const getTotalSalaryActive = (users) => {
+  return users
+    .filter(user => user.active)           // keep only active users
+    .map(user => user.salary || 0)         // extract salary (default 0 if missing)
+    .reduce((total, salary) => total + salary, 0); // sum all salaries
+};
+console.log(getTotalSalaryActive(users));
+
+
+
 
 //--------object operations:
 //1.1 Access object properties
@@ -87,22 +264,21 @@ console.log(users[2].address.city);
 
 const updatedUser = { ...users[0] }; //creates shallow copy using spread operator
 updatedUser.name = "Aman2";
-console.log(updatedUser.name);    //Aman2
-console.log(users[0].name);       //Aman
+console.log(updatedUser.name); //Aman2
+console.log(users[0].name); //Aman
+
+delete updatedUser.name;
+console.log(updatedUser.name);
+
 
 //1.3 Add a new property
-updatedUser.nickname = "Baman";      //adds new propertry nickname to new updatedUser 
-console.log(updatedUser.nickname);  //Baman
-console.log(users[0].nickname) //undefined 
+updatedUser.nickname = "Baman"; //adds new propertry nickname to new updatedUser
+console.log(updatedUser.nickname); //Baman
+console.log(users[0].nickname); //undefined
 
 //1.4 Copy an object using spread operator
-const userCopy = {...users[10]}; //spread operator makes shallow copy
+const userCopy = { ...users[10] }; //spread operator makes shallow copy
 userCopy.salary = 0;
 console.log(userCopy.salary); //0
-console.log(users[10].salary);  //68000 
+console.log(users[10].salary); //68000
 //this shows the original object is not changed with spread operator copy
-
-
-
-
-
