3a92ddd5924e30a8d2b9d97c59ece295c01e8b90
amanlek <amanlekharajani20@gmail.com>
2026-02-17 18:09:23 +0530

    Day -4 Modular js project

--- modular-js-project-yarn.js/src/api.js
+++ modular-js-project-yarn.js/src/api.js
@@ -1,12 +1,11 @@
-import axios from "axios";
+import axiosInstance from "./axiosInstance.js";
 
-export async function fetchUsers() {   //export fetchUsers function
-try {
-    const response = await axios.get(    //get request using axios
-      "https://jsonplaceholder.typicode.com/users"
-    );
+export async function fetchUsers() {
+  try {
+    const response = await axiosInstance.get("/users");
     return response.data;
-  } catch (error) {  //handles errors
+  } catch (error) {
     console.error("Error fetching users:", error.message);
+    throw error;
   }
 }
