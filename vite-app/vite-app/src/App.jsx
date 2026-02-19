b79ae0b41d0c967986f6f4641d0d8ab05ea7cf74
amanlek <amanlekharajani20@gmail.com>
2026-02-19 15:42:16 +0530

    Day 5 vite project

--- vite-app/vite-app/src/App.jsx
+++ vite-app/vite-app/src/App.jsx
@@ -1,6 +1,20 @@
+import { v4 as uuidv4 } from "uuid";
+import dummyImg from "./assets/dummyImg.jpg"; //image imported from asset folder
+
 function App() {
+  const sayHello = import.meta.env.VITE_SAY_HELLO;  //sayHello msg from .env file
+  const apiKey = import.meta.env.VITE_API_KEY;  
+  const id = uuidv4();  //id generated using uuid
   return (
-    <h1>Hello Vite + React 🚀 HMR is Working!</h1>
+    <div>
+        <h2>Vite + React App</h2>
+        <p>Generated UUID: {id}</p>
+        <br />
+        <img src={dummyImg} alt="Logo" width="200" />
+        <br />
+        <p>Msg: {sayHello}</p>
+        <p>Api key: {apiKey}</p>
+    </div>
   )
 }
 export default App
--- vite-app/vite-app/vite.config.js
+++ vite-app/vite-app/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
+  base:/myapp/,
   plugins: [react()],
+  server:{
+    port : 8080,
+  },
+  build: {
+    target: "es2015",
+  },
+
 })
+//import.meta.env.VITE_BASE_PATH || "/",
